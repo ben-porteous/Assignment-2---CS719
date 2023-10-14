615bdf4e1fce11c64d62d5bfd62c44ca005f8e7a
Ben Porteous <bpor008@aucklanduni.ac.nz>
2023-10-15 08:40:53 +1300

    Correction to pokemon-api-routes.js

--- src/routes/api/pokemon-api-routes.js
+++ src/routes/api/pokemon-api-routes.js
@@ -5,11 +5,6 @@ const router = express.Router();
 // When sending a GET request for /:dexNumber (:dexNumber is a path param),
 // return the JSON representation of the correct Pokemon, or return a 404 error
 // if that Pokemon is not found.
-router.get("/test", function (req, res) {
-    res.send("TESTING")
-})
-
-//If client visits/requests /api/pokemon/:dexNumber then individaul json pokemon data returned 
 router.get("/:dexNumber", function (req, res) {
     const pokemonJson = require("../../db/pokemon-db.js")
     if (pokemonJson.getPokemonByDexNumber(req.params.dexNumber)) {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,4 +4,6 @@ const router = express.Router();
 router.use("/", require("./application-routes.js"));
 router.use("/api", require("./api/api-routes.js"));
 
+//this file shoudl have a list of all router locations 
+
 module.exports = router;
